37bb857c1bdbc80020c7e95544a0ccfdc90748e2
TheNerkinz <igorekaterinamushta@gmail.com>
2024-02-17 18:52:34 +0300

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+class navigator{
+    mobileMenuButton = null;
+    mobileMenu = null;
+    mobileMenuButtonClass = 'burger';
+    mobileMenuClass = 'mobile-nav-panel';
+    constructor(){
+        this.init();
+        this.bindEvents();
+    }
+
+    init(){
+        this.mobileMenu = document.querySelector(`.${this.mobileMenuClass}`);
+        this.mobileMinuButton= document.querySelector(`.${this.mobileMinuButtonClass}`);
+    }
+
+    bindEvents(){
+        if(this.mobileMenu == undefined || this.mobileMenuButton == undefined){
+            console.log('Error');
+            return;
+        }
+        this.mobileMenuButton.addEventListener('click', ()=> {
+            console.log(this)
+            this.mobileMenu.classlist.toggle('active');
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    new Navigation()
+})
+
